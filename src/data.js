0a11b5a8093bd008193cb11375f4fd501d3e56c9
ShandrePetersen <shandrepetersen66@gmail.com>
2024-02-29 18:55:13 +0200

    almost done- get button to work

---
+++ src/data.js
@@ -0,0 +1,25 @@
+let shopItemsData = [{
+    id:"greenRolex",
+    name: "Green Rolex",
+    price:  150,
+    descr: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
+    img: "images/Watch1.jpg"
+}, 
+{
+    id:"silverRolex",
+    name: "Silver Rolex",
+    price:  300,
+    descr: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
+    img: "images/Watch2.jpg"}, 
+{
+    id:"blackTag",
+    name: "Black Tag",
+    price:  200,
+    descr: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
+    img: "images/Watch3.jpg"}, 
+{
+    id:"silverTag",
+    name: "Silver Tag",
+    price:  250,
+    descr: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
+    img: "images/Watch4.jpg"}];
